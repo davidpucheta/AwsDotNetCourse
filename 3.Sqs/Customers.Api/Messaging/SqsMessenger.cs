a4cd10a0d57faa5092207f74b07b3de280f2bf6d
David Reyes Pucheta <davidpucheta@gmail.com>
2023-06-20 14:22:38 -0600

    Add project files.

---
+++ 3.Sqs/Customers.Api/Messaging/SqsMessenger.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Options;
+
+namespace Customers.Api.Messaging;
+
+public class SqsMessenger : ISqsMessenger
+{
+    private readonly IAmazonSQS _amazonSQS;
+    private readonly IOptions<QueueSettings> _queueSettings;
+    private string? _queueUrl;
+
+    public SqsMessenger(IAmazonSQS amazonSqs, IOptions<QueueSettings> queueSettings)
+    {
+        _amazonSQS = amazonSqs;
+        _queueSettings = queueSettings;
+    }
+
+    public async Task<SendMessageResponse> SendMessageAsync<T>(T message)
+    {
+        var queueUrl = await GetQueueUrlResponseAsync<T>();
+
+        var sendMessageRequest = new SendMessageRequest()
+        {
+            QueueUrl = queueUrl,
+            MessageBody = JsonSerializer.Serialize(message),
+            MessageAttributes = new Dictionary<string, MessageAttributeValue>()
+            {
+                {
+                    "MessageType",
+                    new MessageAttributeValue()
+                    {
+                        DataType = "String",
+                        StringValue = typeof(T).Name
+                    }
+                }
+            }
+        };
+
+        return await _amazonSQS.SendMessageAsync(sendMessageRequest);
+    }
+
+    private async Task<string> GetQueueUrlResponseAsync<T>()
+    {
+        if (_queueUrl is not null)
+        {
+            return _queueUrl;
+        }
+
+
+        var queueUrlResponse = await _amazonSQS.GetQueueUrlAsync(_queueSettings.Value.Name);
+        _queueUrl = queueUrlResponse.QueueUrl;
+
+        return _queueUrl;
+    }
+}
---
+++ SqsConsumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization.Formatters;
+using System.Threading.Channels;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsConsumer;
+
+var cts = new CancellationTokenSource();
+
+var sqsClient = new AmazonSQSClient();
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var receivedMessageRequest = new ReceiveMessageRequest()
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    AttributeNames = new List<string>{ "All" },
+    MessageAttributeNames = new List<string> { "All" }
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receivedMessageRequest, cts.Token);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine(message.MessageId);
+        Console.WriteLine(message.Body);
+
+        await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+    }
+
+    await Task.Delay(3000);
+}
